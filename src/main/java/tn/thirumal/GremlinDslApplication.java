920749d463493ddae295bdc947bb10cbc58a422a
Thirumal <m.thirumal@hotmail.com>
2022-09-13 21:20:28 +0530

    first commit

---
+++ src/main/java/tn/thirumal/GremlinDslApplication.java
@@ -0,0 +1,13 @@
+package tn.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GremlinDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GremlinDslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/thirumal/GremlinDslApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.thirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GremlinDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
